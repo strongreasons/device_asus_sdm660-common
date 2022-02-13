f8201cae62c18d871673e196dcfd06660c452c4a
Vivekachooz <vivekachooz@gmail.com>
2022-02-13 14:27:14 +0530

    Revert "sdm660-common: Add dummy perf profile."
    This reverts commit f7bcc5396bdbef0a5be14e2cf93b4f8a3b227bd6.
    
    Change-Id: Ib1231392574973a4ab762c262755f6bbdb46673c
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -387,11 +387,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libstdc++.vendor
 
-# QTI Performance
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
-    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
-
 # Secure element
 PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.2.vendor
