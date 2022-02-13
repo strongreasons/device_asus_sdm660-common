aaed8bd11b18ca2a706860fccdb762f801587bb5
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2022-02-13 10:56:31 +0530

    sdm660-common: Include hardware/google/interfaces and hardware/google/pixel
     * Required by power-libperfmgr
    
    Change-Id: I4053938614be26c7066ea83d97955739f39b4144
    Signed-off-by: dotkit <ewprjkt@proton.me>
    Signed-off-by: Kneba <abenkenary3@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -443,7 +443,9 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 # Sqlite
 PRODUCT_PACKAGES += \
