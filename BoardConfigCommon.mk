bb483085f0f941a5d09c80c46e5892fb6e4936cd
Gokul GK <gk@gkcloud.app>
2023-02-08 21:54:27 +0530

    sdm660-common: Use common compatibility matrix
    Change-Id: I485371e3225fafff31646469a008c2d84efbf8eb

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -61,7 +61,6 @@ LOC_HIDL_VERSION := 4.0
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/configs/vintf/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/vintf/manifest.xml
-DEVICE_MATRIX_FILE += device/qcom/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(COMMON_PATH)/configs/vintf/device_framework_compatibility_matrix.xml
 
 ODM_MANIFEST_SKUS += NFC
