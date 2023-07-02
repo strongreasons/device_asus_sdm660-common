8bd591b8ce632502343c6a6a4401f8e3a934f364
strongreasons <abenkenari@gmail.com>
2023-07-02 19:37:20 +0000

    sdm660-common: Drop dead targets
    Change-Id: I71c868d65c47f7b168fa75e2a72dd2758a7b8ef3

--- sdm660.mk
+++ sdm660.mk
@@ -44,8 +44,7 @@ PRODUCT_PACKAGES += \
     libhdmiedid \
     libhfp \
     libsndmonitor \
-    libspkrprot \
-    libssrec
+    libspkrprot
 
 # Audio Configs
 PRODUCT_COPY_FILES += \
@@ -125,8 +124,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.allocator@3.0-impl \
-    android.hardware.graphics.allocator@4.0-impl  \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
@@ -204,10 +201,6 @@ PRODUCT_PACKAGES += \
     libhidltransport.vendor \
     libhwbinder \
     libhwbinder.vendor
-
-# HW crypto
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
 
 # Input
 PRODUCT_COPY_FILES += \
