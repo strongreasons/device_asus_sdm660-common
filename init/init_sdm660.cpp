7b6318616209439772a1fb195040a64652639270
strongreasons <abenkenari@gmail.com>
2023-10-13 09:07:25 +0000

    sdm660-common: init: Update to redfin october FP
    Change-Id: I141fc56d2e0f3fced7430d57e773b2f3eef04a5a

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -95,6 +95,6 @@ void vendor_load_properties()
     set_avoid_gfxaccel_config();
     NFC_check();
 
-    set_build_fingerprint("google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
-    set_build_description("walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys");
+    set_build_fingerprint("google/redfin/redfin:14/UP1A.231005.007/10754064:user/release-keys");
+    set_build_description("redfin-user 13 UP1A.231005.007 10754064 release-keys");
 }
