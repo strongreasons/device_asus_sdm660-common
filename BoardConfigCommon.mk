399bebe416b7f13135da4407c680793b999fadc6
strongreasons <abenkenari@gmail.com>
2024-10-24 18:54:24 +0000

    Revert "sdm660-common: Redone some touchscreen gestures"
    This reverts commit 13394a48ce4c57a9d5568287f07f86079f24f2a9.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -216,7 +216,6 @@ TARGET_BOARD_PLATFORM := sdm660
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/properties/odm.prop
