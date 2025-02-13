AOSPA/android_hardware_qcom_display

Convert libsdmcore to blueprint
Change-Id: I184ba8870e9f92b773cd39b7a82db0bf349ad158

config/display-product.mk
@@ -1,3 +1,7 @@
 ifeq ($(TARGET_USES_YCRCB_CAMERA_ENCODE),true)
 $(call soong_config_set,gralloc,uses_ycrcb_camera_encode,true)
 endif
+
+ifeq ($(TARGET_USES_DRM_PP),true)
+$(call soong_config_set,sdm,uses_drm_pp,true)
+endif

strongreasons/device_asus_sdm660-common

Signed-off-by: Jprimero15 <jprimero15@aospa.co>

config/display-product.mk
@@ -1,3 +1,7 @@
 ifeq ($(TARGET_USES_YCRCB_CAMERA_ENCODE),true)
 $(call soong_config_set,gralloc,uses_ycrcb_camera_encode,true)
 endif
+
+ifeq ($(TARGET_USES_DRM_PP),true)
+$(call soong_config_set,sdmcore,uses_drm_pp,true)
+endif