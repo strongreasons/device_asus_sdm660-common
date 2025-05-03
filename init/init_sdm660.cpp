f8b39f6b82564f5c285c711af6aa40b9aa73ee9e
strongreasons <abenkenari@gmail.com>
2025-05-03 18:31:59 +0000

    sdm660-common: init: Drop unused Safetynet Workaround
    Change-Id: I57d09aeb80c92bb55a22b88a4a85af6421fc6dd7

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -83,13 +83,4 @@ void vendor_load_properties()
 {
     set_avoid_gfxaccel_config();
     NFC_check();
-
-    // Safetynet Workaround
-    property_override("ro.boot.flash.locked", "1");
-    property_override("ro.boot.verifiedbootstate", "green");
-    property_override("ro.boot.veritymode", "enforcing");
-    property_override("ro.boot.vbmeta.device_state", "locked");
-    property_override("ro.build.description", "sdm660_64-user 10 QKQ1 72 release-keys");
-    property_override("ro.build.fingerprint", "asus/ASUS_X00TD/ASUS_X00TD:10/QKQ1/17.2017.2012.438-20201203:user/release-keys");
-    property_override("ro.system.build.fingerprint", "asus/ASUS_X00TD/ASUS_X00TD:10/QKQ1/17.2017.2012.438-20201203:user/release-keys");
 }
