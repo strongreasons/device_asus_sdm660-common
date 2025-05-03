5dbffc2e1c1f75b333bc6dd536b716522cdc3d88
strongreasons <abenkenari@gmail.com>
2025-05-03 17:23:17 +0000

    sdm660-common: Update vendorsetup
    Change-Id: I3f6def5cbee31d4f96e9f912a39d131460cf60a8

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,15 +1,8 @@
 # Clean first
-rm -rf kernel/asus/sdm660
 rm -rf vendor/lineage-priv
 
 # Clean keys
-git clone https://github.com/strongreasons/signing -b rr signing && cp -R signing/* vendor/ && rm -rf signing
-
-# Kernel tree
-git clone --depth=1 --recursive https://github.com/texascake/kernel_asus_sdm660 -b tom/hmp kernel/asus/sdm660
-rm -rf kernel/asus/sdm660/KernelSU/docs
-rm -rf kernel/asus/sdm660/KernelSU/scripts
-rm -rf kernel/asus/sdm660/KernelSU/userspace
-rm -rf kernel/asus/sdm660/KernelSU/website
+mkdir -p vendor/lineage-priv/keys
+git clone https://github.com/electrolaboratory/public-keys vendor/lineage-priv/keys/
 
 export TZ=Asia/Jakarta
