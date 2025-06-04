44d46382bb88ed18d5cca8c58422f3f69c771bfe
strongreasons <abenkenari@gmail.com>
2025-06-04 06:11:05 +0000

    sdm660-common: Regenerate
    Change-Id: I379f19dada9107e7aa72c3b2a6efcef3f6940890

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,24 @@
+#!/bin/bash
+
 # Clean first
 rm -rf vendor/lineage-priv
+rm -rf kernel/asus/sdm660
 
-# Clean keys
+# kernel tree
+git clone --single-branch https://github.com/strongreasons/android_kernel_asus_sdm660 -b folk --depth=1 kernel/asus/sdm660
+
+cd kernel/asus/sdm660
+curl -LSs "https://raw.githubusercontent.com/Sorayukii/KernelSU-Next/stable/kernel/setup.sh" | bash -s hookless
+cd -
+
+# Force Using KSU
+rm -rf system/extras/su
+
+# Sign keys
 mkdir -p vendor/lineage-priv/keys
 git clone https://github.com/electrolaboratory/public-keys vendor/lineage-priv/keys/
 
 export TZ=Asia/Jakarta
+
+rm -rf packages/services/Mms
+git clone https://github.com/electrolaboratory/android_packages_services_Mms packages/services/Mms
