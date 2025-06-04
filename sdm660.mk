2913570709cce4155b88ebf2dcdab409800be78d
SonicBSV <sonicbsv@ya.ru>
2025-06-04 18:19:48 +0300

    sdm660-common: make missing libstagefright_softomx_plugin

--- sdm660.mk
+++ sdm660.mk
@@ -331,7 +331,8 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+    libstagefright_softomx_plugin.vendor
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
