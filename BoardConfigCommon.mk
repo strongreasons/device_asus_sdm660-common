6d0d619463be394c9d8e6bf473227cb874f627a3
Tiktodz <dotkit@electrowizard.me>
2025-06-18 04:15:01 +0000

    sdm660-common: Sort kernel cmdline
    Change-Id: I13a954d343e4049f57bd5aff10270954e567d0a2
    Signed-off-by: texascake <tiktodz@soulvibe-team.cloud>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -141,10 +141,22 @@ BOARD_HAS_QCA_FM_SOC := cherokee
 BOARD_HAVE_QCOM_FM := true
 
 # Kernel
+BOARD_KERNEL_CMDLINE := \
+    androidboot.hardware=qcom \
+    user_debug=31 \
+    msm_rtb.filter=0x37 \
+    ehci-hcd.park=3 \
+    lpm_levels.sleep_disabled=1 \
+    sched_enable_hmp=1 \
+    sched_enable_power_aware=1 \
+    androidboot.boot_devices=soc/c0c4000.sdhci \
+    service_locator.enable=1 \
+    loop.max_part=7 \
+    androidboot.init_fatal_reboot_target=recovery \
+    printk.devkmsg=on \
+    console=ttyMSM0,115200,n8 \
+    androidboot.console=ttyMSM0
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 androidboot.boot_devices=soc/c0c4000.sdhci service_locator.enable=1 loop.max_part=7
-BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery printk.devkmsg=on
-BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
